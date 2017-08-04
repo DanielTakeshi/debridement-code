e903514d4e0a3e189316f43283c48e31f17dd9e8
Sanjay Krishnan <takeshidanny@gmail.com>
2017-08-04 14:39:26 -0700

    Adding calibration images so that if I move camera, I can check the bounding boxes and see if the camera is at the right location.
    I don't know why I didn't do that earlier

---
+++ scripts/check_camera_location.py
@@ -0,0 +1,52 @@
+"""
+The purpose of this script is for me to check the camera location for the dvrk.
+Sometimes I accidentally move the camera, so this will serve to stop that from
+having too damaging effects by saving where the bounding box is stored.
+"""
+
+import environ
+from dvrk.robot import *
+from autolab.data_collector import DataCollector
+import cv2
+import numpy as np
+import pickle
+import sys
+IMDIR = "images/check_calibration/"
+
+
+# Don't change these!!
+xx = 450
+yy = 200
+
+ww = 775
+hh = 700
+
+def initializeRobots():
+    d = DataCollector()
+    r1 = robot("PSM1") # left (but my right)
+    r2 = robot("PSM2") # right (but my left)
+    time.sleep(2)
+    return (r1,r2,d)
+
+
+# todo maybe put some calibration method here to help fix it as needed?
+# if i make a mistake?
+def fix_calibration():
+    pass
+
+
+def save_bounding_box(image):
+    cv2.imwrite(IMDIR+'calibration_blank_image.jpg', image)
+    cv2.rectangle(image, (xx,yy), (xx+ww, yy+hh), (0,255,0), 2)
+    cv2.imshow("Left Camera Image", image)
+    cv2.waitKey(0)
+    cv2.imwrite(IMDIR+'calibration_bbox_image.jpg', image)
+
+
+if __name__ == "__main__":
+    arm1, _, d = initializeRobots()
+    arm1.close_gripper()
+    cv2.imshow("Left Camera Image", d.left_image)
+    cv2.waitKey(0)
+    image = d.left_image.copy()
+    save_bounding_box(image)
