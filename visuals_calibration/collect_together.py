e28f35c87a94b841dcfc2c61e7c4d3582f4354ef
Sanjay Krishnan <takeshidanny@gmail.com>
2017-08-07 11:27:35 -0700

    Slight fix to get stats on distances (in pixel space)

--- visuals_calibration/collect_together.py
+++ visuals_calibration/collect_together.py
@@ -37,6 +37,22 @@ def load_data(filename):
     return data
 
 
+def collect_stats(visuals):
+    """ For distances, we use the usual distance formula for two points. """
+    l2_distances = []
+    for index,val in enumerate(visuals):
+        targ, real = val
+        dist = np.sqrt( (targ[0]-real[0])**2 + (targ[1]-real[1])**2 )
+        l2_distances.append(dist)
+    l2_distances = np.array(l2_distances)
+    print("Distances among the pixels, (x,y) only:")
+    print("mean:   {}".format(l2_distances.mean()))
+    print("median: {}".format(np.median(l2_distances)))
+    print("max:    {}".format(l2_distances.max()))
+    print("min:    {}".format(l2_distances.min()))
+    print("std:    {}".format(l2_distances.std()))
+
+
 def make_fancy_image(image, visuals):
     """ Make a fancy image, e.g. for a paper or presentation.
 
@@ -75,66 +91,5 @@ def make_fancy_image(image, visuals):
 if __name__ == "__main__":
     image_original = cv2.imread(ORIGINAL_IMAGE).copy()
     visuals_data = load_data(IM_VISUALS_FILE)
+    collect_stats(visuals_data)
     make_fancy_image(image_original, visuals_data)
-
-    #for i, (cX, cY, approx, peri) in enumerate(d.left_contours):  
-    #    image = image_original.copy()
-    #    cv2.circle(img=image, center=(cX,cY), radius=50, color=(0,0,255), thickness=1)
-    #    cv2.drawContours(image=image, contours=[approx], contourIdx=-1, color=(0,255,0), thickness=3)
-    #    cv2.imshow("Press ESC if this isn't a desired contour (or a duplicate), press any other key to proceed w/robot movement.", image)
-    #    firstkey = cv2.waitKey(0) 
-
-    #    if firstkey not in ESC_KEYS:
-    #        # First, determine where the robot will move to based on the pixels.
-    #        target = np.squeeze( arm_map.predict([[cX,cY]]) )
-    #        pos = [target[0], target[1], ARM1_ZCOORD]
-    #        rot = tfx.tb_angles(ROTATION[0], ROTATION[1], ROTATION[2])
-
-    #        # Robot moves to that point and will likely be off. I think 6 seconds is enough for the camera to refresh.
-    #        arm.move_cartesian_frame_linear_interpolation(tfx.pose(pos, rot), 0.03)
-    #        time.sleep(6)
-
-    #        # Get the updated image and put the center coordinate there (make it clear). Blue=Before, Red=AfteR.
-    #        updated_image_copy = (d.left_image).copy()
-    #        cv2.circle(img=updated_image_copy, center=(cX,cY), radius=6, color=(255,0,0), thickness=-1)
-    #        cv2.putText(img=updated_image_copy, text="{}".format((cX,cY)), org=(cX,cY), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,0,0), thickness=2)
-
-    #        # Now we apply the callback and drag a box around the end-effector on the (updated!) image.
-    #        window_name = "Robot has tried to move to ({},{}). Click and drag a box around its end effector. Then press any key (or ESC if I made mistake).".format(cX,cY)
-    #        cv2.namedWindow(window_name)
-    #        cv2.setMouseCallback(window_name, click_and_crop)
-    #        cv2.imshow(window_name, updated_image_copy)
-    #        key = cv2.waitKey(0)
-    #        if key in ESC_KEYS:
-    #            continue
-
-    #        # Now save the image with the next available index. It will contain the contours.
-    #        index = len(os.listdir(IMDIR))
-    #        cv2.imwrite(IMDIR+"/point_"+str(index).zfill(2)+".png", updated_image_copy)
-    #        cv2.destroyAllWindows()
- 
-    #        # Get position and orientation of the arm, save, & reset. Be careful that I understand `data_pt` ordering! 
-    #        # I.e. the target_pos is what the random forest predicted for the target position.
-    #        frame = arm.get_current_cartesian_position()
-    #        new_pos = tuple(frame.position[:3])
-    #        new_rot = tfx.tb_angles(frame.rotation)
-    #        new_rot = (new_rot.yaw_deg, new_rot.pitch_deg, new_rot.roll_deg)
-    #        data_pt = {'target_pos': pos,
-    #                   'target_rot': ROTATION,
-    #                   'actual_pos': new_pos,
-    #                   'actual_rot': new_rot,
-    #                   'center_target_pixels': (cX,cY),
-    #                   'center_actual_pixels': CENTER_OF_BOXES[-1]}
-    #        storeData(OUTPUT_FILE, data_pt)
-
-    #        # Some stats for debugging, etc.
-    #        num_added += 1
-    #        print("contour {}, data_pt: {} (# added: {})".format(i, data_pt, num_added))
-    #        assert (2*num_added) == (2*len(CENTER_OF_BOXES)) == len(POINTS)
-
-    #    else:
-    #        print("(not storing contour {})".format(i))
-
-    #    arm.home()
-    #    arm.close_gripper()
-    #    cv2.destroyAllWindows()
